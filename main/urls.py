f49724c08987f2c0ca5b02d482d183364f8f794b
FarahAuraR <farah.rosadi@gmai.com>
2023-09-26 14:44:46 +0700

    bonus tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id,add_item, reduce_item, remove_item
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
+
 
 app_name = 'main'
 
@@ -10,4 +14,10 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+     path('add_item/<int:item_id>/', add_item, name='add_item'),
+    path('reduce_item/<int:item_id>/', reduce_item, name='reduce_item'),
+    path('remove_item/<int:item_id>/',remove_item, name='remove_item'),
 ]
