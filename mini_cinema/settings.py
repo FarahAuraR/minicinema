25060b851ca8500f095aa7371bf720eb4f2b003d
“FarahAuraR” <farah.rosadi@gmail.com>
2023-11-21 23:23:41 +0700

    fix tugas 9

--- mini_cinema/settings.py
+++ mini_cinema/settings.py
@@ -38,8 +38,8 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'main',
-    'authentication'
-    'corsheaders'
+    'authentication',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
