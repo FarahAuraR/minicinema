277b2c65c9368af07ce37bb3195b7addc4a09fa8
“FarahAuraR” <farah.rosadi@gmail.com>
2023-11-22 06:48:19 +0700

    re deploy

--- main/views.py
+++ main/views.py
@@ -46,6 +46,7 @@ def create_item_flutter(request):
         new_product = Item.objects.create(
             user = request.user,
             name = data["name"],
+            amount = int(data["amount"]),
             price = int(data["price"]),
             description = data["description"]
         )
@@ -84,6 +85,7 @@ def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+@csrf_exempt
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -106,6 +108,7 @@ def register(request):
     else:
         return render(request, 'register.html')
 
+@csrf_exempt
 def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -121,6 +124,7 @@ def login_user(request):
     context = {}
     return render(request, 'login.html', context)
 
+@csrf_exempt
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
